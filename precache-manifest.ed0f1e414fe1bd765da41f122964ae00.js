115a6946eb79928fb1eda8bb2d572d3eea20e416
Amitabh Kumar <amitabh.kumar2@gmail.com>
2020-06-17 17:56:01 -0400

    addZ

---
+++ precache-manifest.ed0f1e414fe1bd765da41f122964ae00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d9aa329e44497fdff2a1",
+    "url": "/static/css/main.f8a82c19.chunk.css"
+  },
+  {
+    "revision": "d9aa329e44497fdff2a1",
+    "url": "/static/js/main.d9aa329e.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "adfdb7451b23e01f95b4f67a0c14868b",
+    "url": "/index.html"
+  }
+];
